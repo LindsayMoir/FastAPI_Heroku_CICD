f903357c0e0f1c13dae367b418d4313218aaa3cd
LindsayMoir <tragoes@gmail.com>
2024-09-15 12:36:02 -0700

    Problem with test_quality_metrics. When you upload the images, they get a timestamp from GitHub which is not the timestamp that they got when they were created. As a result testing the time is not beneficial. I removed that code and just test if the files exist

--- tests/test_quality_metrics.py
+++ tests/test_quality_metrics.py
@@ -1,6 +1,5 @@
 import os
 from model import setup_env
-import pandas as pd
 
 
 def test_quality_metrics(y_test=None, predictions=None):
@@ -17,28 +16,3 @@ def test_quality_metrics(y_test=None, predictions=None):
         was not created."
     assert os.path.exists(roc_curve_path), f"File {roc_curve_path} \
         was not created."
-
-    # Check if the files have been created within an appropriate time frame
-    df = pd.read_csv(config['files']['run_log'])
-
-    # Get the start and end times of the last run
-    df = pd.read_csv(config['files']['run_log'])
-    start_time = pd.to_datetime(df.loc[df.index[-1], 'start_time'])
-    end_time = pd.to_datetime(df.loc[df.index[-1], 'end_time'])
-
-    # Get the time the files were last modified
-    conf_matrix_mtime = pd.to_datetime(os.path.getmtime(conf_matrix_path),
-                                       unit='s')
-    roc_curve_mtime = pd.to_datetime(os.path.getmtime(roc_curve_path),
-                                     unit='s')
-
-    # This is unix time which is utc. Just subtract 7 hours (pst) to get
-    # the correct time.
-    conf_matrix_mtime = conf_matrix_mtime - pd.Timedelta(hours=7)
-    roc_curve_mtime = roc_curve_mtime - pd.Timedelta(hours=7)
-
-    # Check if the files were created within the appropriate time frame
-    assert start_time <= conf_matrix_mtime <= end_time, f"\
-        File {conf_matrix_path} not created within the appropriate time frame."
-    assert start_time <= roc_curve_mtime <= end_time, f"\
-        File {roc_curve_path} not created within the appropriate time frame."
