9423e51b4196c3b155b137d7d726f17e755347ac
LindsayMoir <tragoes@gmail.com>
2024-09-15 21:13:34 -0700

    Ran Flake8 and fixed some minor formatting issues

--- tests/main_tests/test_read_root.py
+++ tests/main_tests/test_read_root.py
@@ -4,13 +4,14 @@ from main import app  # Import the FastAPI app
 # Create a TestClient instance with the FastAPI app
 client = TestClient(app)
 
+
 def test_read_root():
     # Send a GET request to the root endpoint
     response = client.get("/")
-    
+
     # Assert that the response status code is 200 (OK)
     assert response.status_code == 200
-    
+
     # Assert that the response JSON matches the expected message
-    assert response.json() == {"message": \
-    "Deploying a ML Model with GBC, FastAPI, Heroku, and DVC"}
+    assert response.json() == {
+        "message": "Deploying a ML Model with GBC, FastAPI, Heroku, and DVC"}
